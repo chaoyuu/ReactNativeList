a807e01592f3d0672e25eff9110cc2cc03b0c84c
chaoyuu <chaoyu995@gmail.com>
2021-06-23 10:10:48 +1000

    Added app name in app.js

--- App.js
+++ App.js
@@ -4,11 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+   <view>
+     <Text> chaoyu</Text>
+   </view>
+  )
 }
 
 const styles = StyleSheet.create({

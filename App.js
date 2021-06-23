022d33534736eab7752ef125b15f774b49e16209
chaoyuu <chaoyu995@gmail.com>
2021-06-23 11:59:11 +1000

    adding dropdown

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import { StyleSheet, Text, View,TextInput,FlatList} from 'react-native';
+import {Dropdown} from './components/Dropdown'
 
-categories = [
+
+
+
+let categories = [
   {label: 'Food', value: 'food'},
   {label: 'Coffee', value: 'coffee'},
   {label: 'Transport', value: 'transport'},
@@ -13,11 +17,24 @@ categories = [
 ]
 
 export default function App() {
+
+  //states
+  const [category,setCategory] =useState()
+  const [amount,setAmount] =useState()
+  const dropdownChange =(value) => {
+    //console.log(value)
+    setCategory(value)
+  }
+  const amountChange =(value) => {
+    //console.log(value)
+    setAmount(value)
+  }
   return (
    <View style={styles.container}>
      <Text> chaoyu</Text>
      <View>
-       <TextInput/>
+       <TextInput keyboardType="number-pad" style={styles.input} onChangeText={amountChange}/>
+       <Dropdown categories={categories} handler={dropdownChange}/>
      </View>
   
    </View>
@@ -29,5 +46,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
    paddingTop: 40,
+  },
+  input: {
+    backgroundColor:'#cccccc',
+    padding:10,
+    fontSize:20,
+
   },
 });
